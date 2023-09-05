c3e02a7ce5e6bceaff1b61495a301aa857de7be3
Gilberts Ahumada <gcabrera@linq.com>
2023-09-05 20:25:02 -0300

    first commit

---
+++ src/HolaMundo.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract HolaMundo {
+    string public saludo = "Hola Mundo!";
+}
---
+++ test/HolaMundo.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {HolaMundo} from "../src/HolaMundo.sol";
+
+contract HolaMundoTest is Test {
+    HolaMundo public holaMundo;
+
+    function setUp() public {
+        holaMundo = new HolaMundo();
+    }
+
+    function testSaludo() public {
+        assertEq(holaMundo.saludo(), "Hola Mundo#");
+    }
+}

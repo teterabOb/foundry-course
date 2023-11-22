70003f475b33edda6d3fdc30d85977e77aadc209
Gilberts Ahumada <gcabrera@linq.com>
2023-11-21 21:29:47 -0300

    factory contract added with its test

---
+++ src/Factory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import {HolaMundo} from "../src/HolaMundo.sol";
+
+error ThrowError();
+
+contract Factory {
+    HolaMundo public holaMundo;
+
+    event Reverting(string indexed message);
+
+    function deploy() external returns(address){
+        HolaMundo newContract = new HolaMundo();
+        holaMundo = newContract;        
+        emit Reverting("Log");
+        return address(newContract);        
+    }
+
+    function revertFunction() pure external {
+        revert ThrowError();
+    }
+}
---
+++ test/Factory.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import {Test} from "forge-std/Test.sol";
+import {Factory} from "../src/Factory.sol";
+
+contract FactoryTest is Test {
+    Factory public factory;
+
+    function setUp() public {
+        factory = new Factory();
+    }
+
+    function test_Deploy() public {
+        address result = factory.deploy();
+        require(result != address(0), "No adddress zero");
+    }
+
+    function testFail_Revert() public view {
+        factory.revertFunction();
+    }
+}

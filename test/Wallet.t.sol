92c0d2ed77fede9aa5b824dd15c88547d43c02bf
Gilberts Ahumada <gcabrera@linq.com>
2023-11-27 19:51:43 -0300

    functions added to wallet test

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -11,10 +11,29 @@ contract WalletTest is Test {
         wallet = new Wallet();
     }
 
+    function _send(uint256 amount) private {
+        payable(address(wallet)).transfer(amount);
+    }
+
+    function _sendLowLevelCall(uint256 amount) private {
+        (bool transfered, ) = address(wallet).call{value: amount}("");
+        require(transfered, "Transfer failed");
+    }
+
     function testDepositEther() public {
         console.logAddress(address(this));
         console.log(address(this).balance);
-        hoax(address(this), 1 ether);
+        hoax(address(this), 10 ether);
         console.log(address(this).balance);
+        address walletAddress = address(wallet);
+        console.log(walletAddress.balance);
+        payable(walletAddress).transfer(1 ether);
+        console.log(walletAddress.balance / 1e18);
+    }
+
+    function testDepositEtherToContract() public {
+        hoax(address(this), 10 ether);
+        _sendLowLevelCall(1 ether);
+        assertEq(address(wallet).balance, 1 ether);
     }
 }

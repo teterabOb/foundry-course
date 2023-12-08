f8a995617335db589c7ac645f4afa1180a6ec0f3
Gilberts Ahumada <gcabrera@linq.com>
2023-12-08 16:14:21 -0300

    test signature done for video

--- test/TestSignature.t.sol
+++ test/TestSignature.t.sol
@@ -4,13 +4,7 @@ pragma solidity 0.8.20;
 import "forge-std/Test.sol";
 
 contract TestSign is Test {
-    struct Wallet {
-      address addr;
-      uint256 publicKeyX;
-      uint256 publicKeyY;
-      uint256 privateKey;
-    }
-
+    
     function testSigneMessage() public {
         vm.skip(true);
         (address alice, uint256 alicePk) = makeAddrAndKey("alice");
@@ -23,11 +17,12 @@ contract TestSign is Test {
         assertEq(signer, alice);
     }
 
-    function testSigneMessageCreateWallet() public {
-        Wallet memory alice = vm.createWallet("alice");
+    function testSigneMessageCreateWallet() public {        
+        address aliceAddress = vm.createWallet("alice").addr;
+        uint256 alicePk = vm.createWallet("alice").privateKey;
         bytes32 hash = keccak256("Signed by Alice");
-        (uint8 v,bytes32 r,bytes32 s) = vm.sign(alice, hash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
         address signer = ecrecover(hash, v, r, s);
-        assertEq(alice.addr, signer);
+        assertEq(aliceAddress, signer);
     }
 }

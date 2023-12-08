a8d9f1fc72335446d58c31de2344cfc82b93741b
Gilberts Ahumada <gilbertsahumada@Gilbertss-MacBook-Pro.local>
2023-12-07 21:09:13 -0300

    test signer added

---
+++ test/TestSignature.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+
+contract TestSign is Test {
+
+    function testSigneMessage() public {
+        (address alice, uint256 alicePk) = makeAddrAndKey("alice");
+        emit log_address(alice);
+        emit log_uint(alicePk);
+
+        bytes32 hash = keccak256("Signed by Alice");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
+        address signer = ecrecover(hash, v, r, s);
+        assertEq(signer, alice);
+    }
+}

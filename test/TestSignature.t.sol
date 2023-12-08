b90a47d6279d39461239adf756f6adb4491a6251
Gilberts Ahumada <gcabrera@linq.com>
2023-12-08 16:29:45 -0300

    variables named changed

--- test/TestSignature.t.sol
+++ test/TestSignature.t.sol
@@ -5,24 +5,24 @@ import "forge-std/Test.sol";
 
 contract TestSign is Test {
     
-    function testSigneMessage() public {
-        vm.skip(true);
+    function testSigneMessage() public {        
         (address alice, uint256 alicePk) = makeAddrAndKey("alice");
         emit log_address(alice);
         emit log_uint(alicePk);
 
-        bytes32 hash = keccak256("Signed by Alice");
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
-        address signer = ecrecover(hash, v, r, s);
+        bytes32 messageHashed = keccak256("Signed by Alice");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, messageHashed);
+        address signer = ecrecover(messageHashed, v, r, s);
         assertEq(signer, alice);
     }
 
     function testSigneMessageCreateWallet() public {        
         address aliceAddress = vm.createWallet("alice").addr;
         uint256 alicePk = vm.createWallet("alice").privateKey;
-        bytes32 hash = keccak256("Signed by Alice");
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, hash);
-        address signer = ecrecover(hash, v, r, s);
+        bytes32 messageHashed = keccak256("Signed by Alice");
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePk, messageHashed);
+        address signer = ecrecover(messageHashed, v, r, s);
+        emit log_address(signer);
         assertEq(aliceAddress, signer);
     }
 }

a1614ad9aa71a71bfd8961da115dea11199d6c5a
Gilberts Ahumada <gcabrera@linq.com>
2024-01-08 19:59:17 -0300

    updating script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    //function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        Counter counter = new Counter();
+        vm.stopBroadcast();            
     }
 }

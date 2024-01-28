9ddf93dd27b4e549e65b6d383ec87cdc10987dfd
Gilberts Ahumada <gcabrera@linq.com>
2024-01-28 17:08:50 -0300

    invariant testing expanded

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -7,5 +7,9 @@ contract ObservdevToken is ERC20 {
 
     constructor() ERC20("Observdev Token", "OBS") {
         _mint(msg.sender, 2_000_000 ether);
+    }
+
+    function mint(address account_, uint256 amount_) external {
+        _mint(account_, amount_);
     }
 }
--- test/Basic4626Deposit.t.sol
+++ test/Basic4626Deposit.t.sol
@@ -1,35 +1,66 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {Test} from "forge-std/Test.sol";
 
-
-contract Basic4626DepositTest is Test {
-
-}
+contract Basic4626DepositTest is Test {}
 
 import {CommonBase} from "forge-std/Base.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import {StdUtils} from "forge-std/StdUtils.sol";
 import {ERC20} from "src/ERC20.sol";
+import {Basic4626Deposit} from "src/Basic4626Deposit.sol";
+import {ObservdevToken} from "src/ERC20.sol";
 
 contract Handler is CommonBase, StdCheats, StdUtils {
     IERC20Like private asset;
+    ObservdevToken private observdevToken;
+    Basic4626Deposit private basicDeposit;
 
-    function setUp() public override {
-        asset = IERC20Like(address(new ERC20()));
+    function setUp() public {
+        observdevToken = new ObservdevToken();
+        asset = IERC20Like(address(observdevToken));
     }
 
-    constructor(address asset_) {
-        asset = IERC20Like(asset_);
+    function deposit(uint256 assets_) public virtual {
+        asset.mint(address(msg.sender), assets_);
+        asset.approve(address(basicDeposit), assets_);
+        uint256 shares = basicDeposit.deposit(assets_, address(this));
     }
 
-    function deposit(uint256 assets_, address receiver_) public returns (uint256){}
-    function transfer(address recipient_, uint256 amount_) public returns (bool) {}    
+    function transfer(address recipient_, uint256 assets_) public virtual {
+        deposit(assets_);
+        basicDeposit.transfer(recipient_, assets_);
+    }
 }
 
 interface IERC20Like {
-    function balanceOf(address) external view returns (uint);
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
-    function transferFrom(address, address, uint) external returns (bool);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
+    function mint(address, uint) external;
 }
